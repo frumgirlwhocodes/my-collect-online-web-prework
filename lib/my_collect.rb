f48709de4e0050248a79b60ed9ae712c3e70de40
lieby weintraub <liebahinda@gmail.com>
2019-02-04 01:03:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
-  i=0 
-  new_array= []
- while i < array.size
-new_array << yield(array[i]) 
-end 
-new_array
-end 
+  i = 0
+  new_array = Array.new
+  while i < array.size
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
